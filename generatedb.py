fdb155f3d7f05d3b47b02e72bf0c265862bc4a3d
roald <roald.ong@gmail.com>
2022-09-22 00:00:55 +0800

    Added in a new column for time

--- generatedb.py
+++ generatedb.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('sensordata.db')
 curs = connection.cursor()
-curs.execute('''CREATE TABLE IF NOT EXISTS sensordata (Nodeid INT, Battery INT, X INT, Y INT, Z INT)''')
+curs.execute('''CREATE TABLE IF NOT EXISTS sensordata (Nodeid INT, Battery INT, X INT, Y INT, Z INT, Time DOUBLE)''')
 
 connection.commit()
 connection.close()
